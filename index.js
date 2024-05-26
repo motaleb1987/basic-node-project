4bd920dce1b9fd97d5ec1fb863f4b07ef6150bb4
muhin-khan <muhincse11@gmail.com>
2024-05-26 12:07:00 +0600

    Local module, exports, require

---
+++ index.js
@@ -0,0 +1,11 @@
+// ------all funciton import---
+//const student=require('./students');
+const {getName, getAge,cgpa}=require('./students');
+
+// console.log(student.getName());
+// console.log(student.getAge());
+// console.log(student.cgpa());
+
+console.log(getName());
+console.log(getAge());
+console.log(cgpa());
---
+++ students.js
@@ -0,0 +1,25 @@
+// -----export system-3
+exports.getName=()=>{
+    return "Motaleb Hossain";
+}
+
+exports.getAge=()=>{
+    return 35;
+}
+exports.cgpa=()=>{
+    return 3.97;
+}
+
+
+
+// -----export system-1
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.cgpa = cgpa;
+
+// -----export system-2
+// module.exports={
+//     getName,
+//     getAge,
+//     cgpa
+// }
